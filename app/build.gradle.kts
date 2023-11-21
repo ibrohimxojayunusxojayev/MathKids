99531a4f11d1743d5f6e5385d8fd6a6c2d1bac78
User-1 <bestthebest2007@gmail.com>
2023-11-21 07:58:15 +0300

    21.11.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("io.coil-kt:coil-gif:2.2.2")
 
+    //eval
+    implementation ("org.mariuszgromada.math:MathParser.org-mXparser:4.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/uz/itschool/mathkidsnew/screens/questionscreen/QuestionScreen.kt
+++ app/src/main/java/uz/itschool/mathkidsnew/screens/questionscreen/QuestionScreen.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -22,10 +26,18 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import org.mariuszgromada.math.mxparser.Expression
 import uz.itschool.mathkidsnew.R
+import kotlin.random.Random
+
+val SIGNS = listOf('+', '-', '*', '/')
 
 @Composable
 fun QuestionScreen(navController: NavController) {
+    val equation = remember { generateEquation() }
+    val answer = remember { equation.calculate() }
+    var userGuess by remember { mutableStateOf("") }
+    var resultMessage by remember { mutableStateOf("") }
     Box(
         modifier = with(Modifier) {
             fillMaxSize()
@@ -49,12 +61,81 @@ fun QuestionScreen(navController: NavController) {
                     .padding(10.dp),
                 horizontalArrangement = Arrangement.Center
             ) {
-                Text(text = "1", color = Color.White, fontSize = 45.sp)
-                Text(text = " * ", color = Color.White, fontSize = 45.sp)
-                Text(text = "2", color = Color.White, fontSize = 45.sp)
-                Text(text = " = ", color = Color.White, fontSize = 45.sp)
-                Text(text = "2", color = Color.White, fontSize = 45.sp)
+                Text(text = "$equation", color = Color.White, fontSize = 45.sp)
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.plus_btn), contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("question_screen")
+                            userGuess = "+"
+                        }
+                        .height(80.dp)
+                        .width(80.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.minus_btn), contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("question_screen")
+                            userGuess = "-"
+                        }
+                        .height(80.dp)
+                        .width(80.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.multi_btn), contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("question_screen")
+                            userGuess = "*"
+                        }
+                        .height(80.dp)
+                        .width(80.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.div_btn), contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate("question_screen")
+                            userGuess = "/"
+                            resultMessage = evaluateAndDisplayResult(userGuess, answer)
+                        }
+                        .height(80.dp)
+                        .width(80.dp)
+                )
             }
         }
     }
+
+}
+fun evaluateAndDisplayResult(userGuess: String, answer: Double): String {
+    if (userGuess in SIGNS) {
+        val expression = Expression(equation.replace(" ", userGuess))
+        if (expression.calculate() == answer) {
+            return "Correct!"
+        } else {
+            return "Incorrect."
+        }
+    } else {
+        return "Invalid input."
+    }
+}
+fun generateEquation(): Expression {
+    val a = (1..9).random()
+    val b = (1..9).random()
+    return Expression("$a + $b")
 }
